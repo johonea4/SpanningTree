ea024effc7329029da744d921b6658d2040cdb91
Joseph M. Honea <johonea@outlook.com>
2018-01-27 17:31:33 -0600

    Complete

--- Switch.py
+++ Switch.py
@@ -20,8 +20,7 @@
 # self.topology (link to the greater topology structure used for message passing)
 #
 # Copyright 2016 Michael Brown, updated by Kelly Parks
-#           Based on prior work by Sean Donovan, 2015
-			    												
+#           Based on prior work by Sean Donovan, 2015	    												
 
 from Message import *
 from StpSwitch import *
@@ -38,12 +37,16 @@ class Switch(StpSwitch):
         self.data = dict()
         self.data['root'] = idNum
         self.data['distance'] = 0
-        self.data['actives'] = []
+        self.data['actives'] = set()
         self.data['passthru'] = idNum 
+        self.data['ispassthru'] = False
 
-        self.distLinks = dict()
+        self.linkdata = dict()
         for l in self.links:
-            self.distLinks[l] = 0
+            self.linkdata[l] = dict()
+            self.linkdata[l]['root'] = None
+            self.linkdata[l]['distance'] = None
+            self.linkdata[l]['ispassthru'] = None
 
 
     def send_initial_messages(self):
@@ -60,12 +63,59 @@ class Switch(StpSwitch):
     def process_message(self, message):
         #TODO: This function needs to accept an incoming message and process it accordingly.
         #      This function is called every time the switch receives a new message.
-        s = "Root: " + str(message.root)
-        s += " Distance: " + str(message.distance)
-        s += " Origin: " + str(message.origin)
-        s += " Destination: " + str(message.destination)
-        s += " PassThrough: " + str(message.pathThrough)
-        print(s)
+
+        '''
+        Implementation:
+
+        As messages arrive, update the info structures that contain the information for the 
+        node's neighbors. After the information is updated, update the node's information
+        accordingly. Then, as the root or passthru changed send messages reflecting the
+        change to its neighbor's
+        '''
+
+        self.linkdata[message.origin]['root'] = message.root
+        self.linkdata[message.origin]['distance'] = message.distance
+        self.linkdata[message.origin]['ispassthru'] = message.pathThrough
+
+        rootChanged = False
+        passThruChanged = False
+        isPassThruChanged = False
+
+        ispassthru = self.data['ispassthru']
+
+        for l in self.links:
+            if self.linkdata[l]['root'] is None:
+                continue;
+            if self.linkdata[l]['root'] < self.data['root']:
+                self.data['root'] = self.linkdata[l]['root']
+                self.data['distance'] = self.linkdata[l]['distance']+1
+                self.data['passthru'] = l
+                rootChanged = True
+            if self.linkdata[l]['root'] == self.data['root'] and self.linkdata[l]['distance'] + 1 < self.data['distance']:
+                self.data['distance'] = self.linkdata[l]['distance']+1
+                self.data['passthru'] = l
+                passThruChanged = True
+            if self.linkdata[l]['root'] == self.data['root'] and self.linkdata[l]['distance'] + 1 == self.data['distance'] and l < self.data['passthru']:
+                self.data['passthru'] = l
+                passThruChanged = True
+
+        for l in self.links:
+            if l == self.data['root']:
+                self.data['actives'].add(l)
+            elif l == self.data['passthru']:
+                self.data['actives'].add(l)
+            elif self.linkdata[l]['ispassthru']:
+                self.data['actives'].add(l)
+            elif not self.linkdata[l]['ispassthru'] and l in self.data['actives']:
+                self.data['actives'].remove(l)
+
+        if rootChanged or passThruChanged:
+            for l in self.links:
+                ispassthru = self.data['passthru'] == l
+                m = Message(self.data['root'],self.data['distance'],self.switchID,l,ispassthru)
+                self.send_message(m)
+        
+
         return
         
     def generate_logstring(self):
@@ -80,4 +130,15 @@ class Switch(StpSwitch):
         #      for switch 2 would have the following text:
         #      2 - 1, 2 - 3
         #      A full example of a valid output file is included (sample_output.txt) with the project skeleton.
-        return "switch logstring"
+        
+        self.data['actives'] = sorted(self.data['actives'])
+        log = ""
+
+        for a in self.data['actives']:
+            log += str(self.switchID) + " - " + str(a) + ", "
+        
+        if len(log) > 0:
+            log = log[:-2]
+
+
+        return log
